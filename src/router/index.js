d01d64f88f0352ef0508f538517db9418a3e94d2
AdrianGerman <adrian.german1019@gmail.com>
2023-12-15 08:33:54 -0600

    named routes

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: HomeView },
-    { path: '/about', component: () => import('@/views/AboutView.vue') },
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/about', name: 'about', component: () => import('@/views/AboutView.vue') },
     {
       path: '/chats',
       component: () => import('@/views/ChatsView.vue'),
